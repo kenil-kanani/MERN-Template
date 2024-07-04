100c3637e8e23ed6ccdbd79630a2446b5e0fbe99
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-07-05 01:21:11 +0530

    cors, cookies config modified, code formatter added

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,5 +1,7 @@
 export const USER_ROLES = ['user', 'admin'];
 
+export const DB_NAME = "db_name";
+
 export const AUTH_ERRORS = {
     EMAIL_ALREADY_REGISTERED: 'Email already registered',
     REPOSITORY_LAYER: 'Something went wrong in the auth repository',
